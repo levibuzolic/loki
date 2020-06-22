levibuzolic/loki

commit 03d33ae944e807ca739df9d98cb8aa0039a1c9cc

Add isLokiRunning() (#246)

packages/integration-react-native/src/configure-storybook-react-native.js
@@ -112,6 +112,10 @@ async function configureStorybook() {
   };
 
   const restore = () => {
+    if ('loki' in global) {
+      global.loki.isRunning = true;
+    }
+
     customErrorHandler = null;
     ReactNative.StatusBar.setHidden(originalState.statusBarHidden);
     // eslint-disable-next-line no-console
@@ -119,6 +123,11 @@ async function configureStorybook() {
   };
 
   const prepare = () => {
+    if ('loki' in global) {
+      global.loki = {};
+    }
+    global.loki.isRunning = true;
+
     customErrorHandler = async (error, isFatal) => {
       if (isFatal) {
         emit('error', {

examples/react/src/IsLokiRunning.js
@@ -1,0 +1,34 @@
+import React from 'react';
+import isLokiRunning from '@loki/is-loki-running';
+import './isLokiRunning.css';
+
+const DisableAnimationsContext = React.createContext(false);
+
+export const withDisabledAnimations = getStory => (
+  <DisableAnimationsContext.Provider value={isLokiRunning()}>
+    {getStory()}
+  </DisableAnimationsContext.Provider>
+);
+
+const IsLokiRunning = () => {
+  const disableAnimations = React.useContext(DisableAnimationsContext);
+  const running = isLokiRunning();
+
+  return (
+    <div className="is-loki-running">
+      <code
+        style={{
+          background: disableAnimations ? 'green' : 'red',
+        }}
+      >
+        disableAnimations={JSON.stringify(disableAnimations)}
+      </code>
+      <code style={{ background: running ? 'green' : 'red' }}>
+        isLokiRunning={JSON.stringify(running)}
+      </code>
+      <code className="loki-attribute">*[loki-test]</code>
+    </div>
+  );
+};
+
+export default IsLokiRunning;

examples/react/src/isLokiRunning.css
@@ -1,0 +1,22 @@
+.is-loki-running code {
+  display: inline-block;
+  color: white;
+  margin-right: 1em;
+  padding: 0.5em;
+}
+
+.loki-attribute {
+  background: red;
+}
+
+.loki-attribute::after {
+  content: '=false';
+}
+
+*[loki-test] .loki-attribute {
+  background: green;
+}
+
+*[loki-test] .loki-attribute::after {
+  content: '=true';
+}

examples/react/src/stories/index.stories.js
@@ -17,6 +17,7 @@ import FetchComponent from '../FetchComponent';
 import ZeroHeightWithPadding from '../ZeroHeightWithPadding';
 import Hover from '../Hover';
 import FocusedInput from '../FocusedInput';
+import IsLokiRunning, { withDisabledAnimations } from '../IsLokiRunning';
 
 storiesOf('Welcome', module).lokiSkip('to Storybook', () => (
   <Welcome showApp={linkTo('Button')} />
@@ -76,3 +77,7 @@ storiesOf('Zero height', module).add('with padding', () => (
 storiesOf('Hover', module).add('default', () => <Hover />);
 
 storiesOf('FocusedInput', module).add('default', () => <FocusedInput />);
+
+storiesOf('isLokiRunning()', module)
+  .addDecorator(withDisabledAnimations)
+  .add('default', () => <IsLokiRunning />);

packages/browser/src/add-loki-session-marker.js
@@ -1,7 +1,0 @@
-const addLokiSessionMarker = window => {
-  window.document.querySelector(':root').setAttribute('loki-test', true);
-  /* eslint-disable no-underscore-dangle, no-param-reassign */
-  window._isLokiTest = true;
-};
-
-module.exports = addLokiSessionMarker;

commit f78bcd199cfddb93f496fa4ec99e95a7e7c0307b

Fix loki global check for React Native
Fix a bug introduced in #246 where the check for the loki object being defined on global was inverted

packages/integration-react-native/src/configure-storybook-react-native.js
@@ -112,7 +112,7 @@ async function configureStorybook() {
   };
 
   const restore = () => {
-    if ('loki' in global) {
+    if (!'loki' in global) {
       global.loki.isRunning = true;
     }
 
@@ -123,7 +123,7 @@ async function configureStorybook() {
   };
 
   const prepare = () => {
-    if ('loki' in global) {
+    if (!'loki' in global) {
       global.loki = {};
     }
     global.loki.isRunning = true;